7c83ce4df0c4d954e81ea3a2d0e526e0f5fd193b
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-07-15 01:44:12 +0600

    Fix hydration warning browser extensions on body tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body suppressHydrationWarning className="min-h-full flex flex-col">
         <CartProvider>
           <Navbar />
           <main className="flex-1">{children}</main>
